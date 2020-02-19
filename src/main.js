5866af5977768d0534693c23b11684c28cf7a8c7
blacksonic <soos.gabor86@gmail.com>
2020-02-19 18:08:15 +0100

    feat(vue3): trying out version 3

--- src/main.js
+++ src/main.js
@@ -1,13 +1,8 @@
-import Vue from 'vue';
+import { createApp } from 'vue';
 import Vuex from 'vuex';
 import App from './components/App.vue';
 import 'todomvc-app-css/index.css';
-import { storeFactory } from './store/factory';
 
-Vue.config.productionTip = false;
-Vue.use(Vuex);
-
-new Vue({
-  store: storeFactory(),
-  render: h => h(App)
-}).$mount('app-root');
+const app = createApp(App);
+app.use(Vuex);
+app.mount('app-root');
